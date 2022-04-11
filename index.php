8b58eac77fad6c1136ce6fc639e0dd31d912c91e
alisadewo <sadewoali13@gmail.com>
2022-04-11 12:42:55 +0700

    Menambahkan list di index.php

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti maiores sit adipisci placeat dolorum blanditiis illo enim consectetur minima quidem perferendis totam esse debitis iusto quis, quisquam ut, iure at?</p>
+    <li>
+        <ul>satu</ul>
+        <ul>dua</ul>
+    </li>
 </body>
 </html>
